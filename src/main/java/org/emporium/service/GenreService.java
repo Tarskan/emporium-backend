0209d33c08995885bb11b7e178f162449f349360
Tarskan <55380090+Tarskan@users.noreply.github.com>
2023-05-09 13:25:50 +0200

    :beers: Forgotten part of Genre

---
+++ src/main/java/org/emporium/service/GenreService.java
@@ -0,0 +1,53 @@
+package org.emporium.service;
+
+import org.emporium.model.GenericCreateDTO;
+import org.emporium.model.GenericModifyDTO;
+import org.emporium.model.Genre;
+import org.emporium.repository.GenreRepository;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.List;
+
+@Singleton
+@Service
+public class GenreService {
+    @Inject
+    GenreRepository genreRepository;
+
+    public List<Genre> getAllGenre() {
+        return genreRepository.findAll();
+    }
+
+    public Genre getByIdGenre(String idGenre) throws Exception {
+        return genreRepository.findById(idGenre).orElseThrow(() -> new Exception("Genre not found."));
+    }
+
+    public Genre addGenre(GenericCreateDTO type) {
+        Genre genreNew = Genre.builder()
+                .name(type.name)
+                .build();
+
+        return genreRepository.save(genreNew);
+    }
+
+    public Genre modifyGenre(GenericModifyDTO genre) {
+        if (genreRepository.existsById(genre.getId())) {
+            Genre genreModified = Genre.builder()
+                    .idGenre(genre.getId())
+                    .name(genre.getName())
+                    .build();
+
+            return genreRepository.save(genreModified);
+        } else {
+            throw new IllegalArgumentException("Id: " + genre.getId() + " Non trouvée dans la bdd");
+        }
+    }
+
+    public String suppGenre(String idGenre) throws Exception {
+        Genre genreToDelete = genreRepository.findById(idGenre).orElseThrow(() -> new Exception("Id " + idGenre + " n'existe pas ou a deja était supprimer"));
+        genreRepository.delete(genreToDelete);
+        return "Le genre a était supprimer";
+    }
+}
