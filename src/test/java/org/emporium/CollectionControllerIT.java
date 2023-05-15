023265b5b5d9122608d3f43c7ecb109d452477f0
Tarskan <55380090+Tarskan@users.noreply.github.com>
2023-05-15 08:57:14 +0200

    ✅ Add test for collection

---
+++ src/test/java/org/emporium/CollectionControllerIT.java
@@ -0,0 +1,7 @@
+package org.emporium;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class CollectionControllerIT extends CollectionControllerTest {
+}
---
+++ src/test/java/org/emporium/CollectionControllerTest.java
@@ -0,0 +1,7 @@
+package org.emporium;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class CollectionControllerTest {
+}
