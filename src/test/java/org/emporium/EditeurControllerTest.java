034366b98955cbf9815a6f8733f235c0cf17c6b0
Tarskan <55380090+Tarskan@users.noreply.github.com>
2023-05-15 08:55:58 +0200

    ✅ Add test for Editeur

---
+++ src/test/java/org/emporium/EditeurControllerTest.java
@@ -0,0 +1,82 @@
+package org.emporium;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+public class EditeurControllerTest {
+    @Test
+    public void testAllEditeurEndPoint() {
+        given()
+                .when().get("/editeur")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetEditeurByIdEditeurEndPoint() {
+        given()
+                .when().get("/editeur/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testGetEditeurSearchByName() {
+        given()
+                .when().get("/editeur/search/Tourbillon")
+                .then()
+                .statusCode(200);
+    }
+
+    /*@Test
+    public void testModifyOeuvresEndPoint() {
+        OeuvresModifyDTO oeuvresModifyDTO = new OeuvresModifyDTO();
+        oeuvresModifyDTO.setIdOeuvre("1");
+        oeuvresModifyDTO.setDescription("test");
+        oeuvresModifyDTO.setIdAuteur("1");
+        oeuvresModifyDTO.setIdGenre("1");
+        oeuvresModifyDTO.setIdSupport("2");
+        oeuvresModifyDTO.setIdType("1");
+        oeuvresModifyDTO.setImage("1");
+        oeuvresModifyDTO.setSousTitre("test");
+        oeuvresModifyDTO.setTitre("test");
+
+        given()
+                .body(oeuvresModifyDTO)
+                .header("Content-Type", "application/json")
+                .when().put("/oeuvres")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void testAddOeuvresEndPoint() {
+        OeuvresCreateDTO oeuvresCreateDTO = new OeuvresCreateDTO();
+        oeuvresCreateDTO.setDescription("test");
+        oeuvresCreateDTO.setIdAuteur("1");
+        oeuvresCreateDTO.setIdGenre("1");
+        oeuvresCreateDTO.setIdSupport("2");
+        oeuvresCreateDTO.setIdType("1");
+        oeuvresCreateDTO.setImage("1");
+        oeuvresCreateDTO.setSousTitre("test");
+        oeuvresCreateDTO.setTitre("test");
+
+        given()
+                .body(oeuvresCreateDTO)
+                .header("Content-Type", "application/json")
+                .when().post("/oeuvres")
+                .then()
+                .statusCode(200);
+    }
+
+    */@Test
+    public void testSuppEditeurEndPoint() {
+        given()
+                .when().delete("/editeur/delete/1")
+                .then()
+                .statusCode(200);
+    }
+}
