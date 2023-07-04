3fcf66eb100998d1da5123a2ddd50802904197b5
Tarskan <55380090+Tarskan@users.noreply.github.com>
2023-07-04 17:29:15 +0200

    :construction: Progress on security

--- src/main/java/org/emporium/controller/AuteurController.java
+++ src/main/java/org/emporium/controller/AuteurController.java
@@ -7,11 +7,14 @@ import org.emporium.service.AuteurService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 @Controller
 @Path("/auteur")
@@ -25,12 +28,13 @@ public class AuteurController {
     @GET
     @RolesAllowed("User")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAllAuteur() {
+    public Response getAllAuteur(@Context SecurityContext securityContext) {
         return auteurService.getAllAuteur();
     }
 
     @Path("/{idAuteur}")
     @GET
+    @PermitAll
     @Produces(MediaType.APPLICATION_JSON)
     public Response getByIdAuteur(@PathParam("idAuteur") String idAuteur) throws Exception {
         return auteurService.getByIdAuteur(idAuteur);
@@ -38,6 +42,7 @@ public class AuteurController {
 
     @Path("/search/{auteur}")
     @GET
+    @PermitAll
     @Produces(MediaType.APPLICATION_JSON)
     public Response RechercheUtilisateurByPseudo(@PathParam("auteur") String auteur) {
         return auteurService.getAuteurAutocomplete(auteur);
